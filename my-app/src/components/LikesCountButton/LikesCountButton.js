6344805a3002c48c7db7a546e552b854b089c77b
TeitokuRoss <sanyador2014@gmail.com>
2022-04-02 13:16:27 +0300

    Button

--- my-app/src/components/LikesCountButton/LikesCountButton.js
+++ my-app/src/components/LikesCountButton/LikesCountButton.js
@@ -1,3 +1,22 @@
+import { Component } from "react";
 import styles from "./LikesCountButton.module.css";
 
+class LikesCountButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likesCount: 0,
+    };
+  }
+  increasLikes = (e) => {
+    const { likesCount } = this.state;
+    const { step } = this.props;
+    this.setState({ likesCount: likesCount + step });
+  };
+  render() {
+    const { likesCount } = this.state;
+    return <button onClick={this.increasLikes}>+</button>;
+  }
+}
 export default LikesCountButton;
